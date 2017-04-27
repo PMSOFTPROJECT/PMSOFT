6fea04b2cddc96f27b889c27107f70f144bbdced
Julian Castañeda <Julian Castaeda>
2017-04-27 12:28:47 -0500

    Se agregaron los scripts de la base de datos Crear_Version y Crear_Tipo_Recurso

---
+++ bd/02_SQL_Crear_Tipo_Recurso.sql
@@ -0,0 +1,22 @@
+﻿-- Table: public.tiporecurso
+
+-- DROP TABLE public.tiporecurso;
+
+CREATE TABLE public.tiporecurso
+(
+  tiporecurso_id bigint NOT NULL,
+  tiporecurso_editable boolean NOT NULL,
+  tiporecurso_estado character varying(255) NOT NULL,
+  tiporecurso_fechacreacion timestamp without time zone NOT NULL,
+  tiporecurso_fechaultimocambio timestamp without time zone NOT NULL,
+  tiporecurso_nombre character varying(255) NOT NULL,
+  tiporecurso_usuariocreacion character varying(255) NOT NULL,
+  tiporecurso_usuarioultimocambio character varying(255) NOT NULL,
+  tiporecurso_version integer,
+  CONSTRAINT tiporecurso_pkey PRIMARY KEY (tiporecurso_id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.tiporecurso
+  OWNER TO postgres;
---
+++ bd/02_SQL_Crear_Version.sql
@@ -0,0 +1,25 @@
+﻿-- Table: public.version
+
+-- DROP TABLE public.version;
+
+CREATE TABLE public.version
+(
+  version_id bigint NOT NULL,
+  version_comentario character varying(255) NOT NULL,
+  version_editable boolean NOT NULL,
+  version_estado character varying(255) NOT NULL,
+  version_fechacreacion timestamp without time zone NOT NULL,
+  version_fechafin timestamp without time zone NOT NULL,
+  version_fechainicio timestamp without time zone NOT NULL,
+  version_fechaultimocambio timestamp without time zone NOT NULL,
+  version_nombre character varying(255) NOT NULL,
+  version_usuariocreacion character varying(255) NOT NULL,
+  version_usuarioultimocambio character varying(255) NOT NULL,
+  version_version integer,
+  CONSTRAINT version_pkey PRIMARY KEY (version_id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.version
+  OWNER TO postgres;
