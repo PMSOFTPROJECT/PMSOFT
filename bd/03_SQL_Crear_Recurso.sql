2794ef23088044b955ac9a20af698c370d872829
Julian Castañeda <Julian Castaeda>
2017-05-24 01:04:54 -0500

    Se agregó .sql de Recurso, falta crear relación con TipoRecurso

---
+++ bd/03_SQL_Crear_Recurso.sql
@@ -0,0 +1,24 @@
+﻿-- Table: public.recurso
+
+-- DROP TABLE public.recurso;
+
+CREATE TABLE public.recurso
+(
+  recurso_id bigint NOT NULL,
+  recurso_costo double precision NOT NULL,
+  recurso_editable boolean NOT NULL,
+  recurso_estado integer NOT NULL,
+  recurso_fechacreacion timestamp without time zone NOT NULL,
+  recurso_fechaultimocambio timestamp without time zone NOT NULL,
+  recurso_nombre character varying(255) NOT NULL,
+  recurso_usuariocreacion character varying(255) NOT NULL,
+  recurso_usuarioultimocambio character varying(255) NOT NULL,
+  recurso_version integer,
+  CONSTRAINT recurso_pkey PRIMARY KEY (recurso_id),
+  CONSTRAINT recurso_recurso_nombre_key UNIQUE (recurso_nombre)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.recurso
+  OWNER TO postgres;
